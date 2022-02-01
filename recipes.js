828016ce5f40a566ace80218b3a3057d2e4f5db2
Tim-Rohner <timroh92@gmail.com>
2022-02-01 16:41:51 +0100

    modifies recipes.js adding Halloumi

--- recipes.js
+++ recipes.js
@@ -1,7 +1,7 @@
-const foodArray = ['Pizza', 'Burger', 'Salad', 'Doener', 'Pasta'];
+const foodArray = ['Pizza', 'Burger', 'Salad', 'Doener', 'Pasta', 'Halloumi'];
 
 const randomFood = () => {
-    console.log(foodArray[Math.floor(Math.random()*5)])
+    console.log(foodArray[Math.floor(Math.random()*6)])
 }
 
 randomFood()
